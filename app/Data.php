4e9907f8ccd37ab2aa9b6f67065ab26a3d4656fd
Your Name <you@example.com>
2019-01-10 17:06:57 +0800

    's'

--- app/Data.php
+++ app/Data.php
@@ -15,7 +15,7 @@ class Data extends Authenticatable
         $host = $_SERVER['HTTP_HOST'];
         $domain =  str_after($host,'.');
         $host =  Host::where(['name'=>$domain])->first();
-        return Data::where(['host_id'=>$host->id])->take($count)->orderby('id','desc')->get();
+        return Data::where(['type_id'=>$host->type_id])->take($count)->orderby('id','desc')->get();
     }
 
     public static function RandPageList($count)
@@ -23,10 +23,49 @@ class Data extends Authenticatable
         $host = $_SERVER['HTTP_HOST'];
         $domain =  str_after($host,'.');
         $host =  Host::where(['name'=>$domain])->first();
-        return Data::where(['data.host_id'=>$host->id])
+        return Data::where(['data.type_id'=>$host->type_id])
             ->join('nav', 'data.nav_id', '=', 'nav.id')
             ->take($count)
             ->select('data.id','data.title','nav.en_name')
             ->get();
     }
+
+    public static function BannerList($count){
+        $host = $_SERVER['HTTP_HOST'];
+        $domain =  str_after($host,'.');
+        $host =  Host::where(['name'=>$domain])->first();
+        return Data::orderByRaw("RAND()")->where(['data.type_id'=>$host->type_id])
+            ->join('nav', 'data.nav_id', '=', 'nav.id')
+            ->orderby('id','desc')
+            ->take($count)
+            ->select('data.id','data.title','nav.en_name')
+            ->get();
+    }
+
+    public static function 随机日期()
+    {
+        return date("Y-m-d",strtotime("-".rand(0,5)." day"));
+    }
+
+    public static function 人名()
+    {
+        $data = file(public_path('author.txt'));
+        return $data;
+    }
+
+    public function 地名()
+    {
+        $data = file(public_path('city.txt'));
+        return $data;
+    }
+
+    public function 栏目()
+    {
+        $host = $_SERVER['HTTP_HOST'];
+        $domain =  str_after($host,'.');
+        $host =  Host::where(['name'=>$domain])->first();
+        $nav =  Nav::where(['host_id'=>$host->id])->get();
+        return $nav;
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@
 */
 
 Route::get('/','HomeController@index');
+Route::get('{id}.html','HomeController@show');
